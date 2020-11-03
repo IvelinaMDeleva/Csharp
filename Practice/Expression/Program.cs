13a7392b96a1bbbf623b6354e2ec8db466f2e7dc
IvelinaMDeleva <73790748+IvelinaMDeleva@users.noreply.github.com>
2020-11-03 19:58:14 +0100

    Added Exression project; for and while loops

---
+++ Practice/Expression/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print numbers from 100 to 1.
+            for (int number = 100; number >= 1; number--)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Print numbers from 1 to 100.
+            int counter = 1;
+            while (counter <= 100)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+        }
+    }
+}
