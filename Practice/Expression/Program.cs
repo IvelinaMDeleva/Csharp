4730c974ed632449a5171917b280c68c53bc7e99
IvelinaMDeleva <73790748+IvelinaMDeleva@users.noreply.github.com>
2020-11-04 21:17:51 +0100

    Added password input check program.

--- Practice/Expression/Program.cs
+++ Practice/Expression/Program.cs
@@ -7,18 +7,34 @@ namespace Expression
         static void Main(string[] args)
         {
             //Print numbers from 100 to 1.
-            for (int number = 100; number >= 1; number--)
-            {
-                Console.WriteLine(number);
-            }
+            //for (int number = 100; number >= 1; number--)
+            //{
+            //    Console.WriteLine(number);
+            //}
 
-            //Print numbers from 1 to 100.
-            int counter = 1;
-            while (counter <= 100)
+            ////Print numbers from 1 to 100.
+            //int counter = 1;
+            //while (counter <= 100)
+            //{
+            //    Console.WriteLine(counter);
+            //    counter++;
+            //}
+
+            //string password = "Aira";
+            var input = Console.ReadLine();
+            //if (input == "Aira")
+            //{
+            //    Console.WriteLine("Hello my friend!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wrong password");
+            //}
+            while (input != "Aira")
             {
-                Console.WriteLine(counter);
-                counter++;
+                input = Console.ReadLine();
             }
+            Console.WriteLine("Hello my friend!");
         }
     }
 }
